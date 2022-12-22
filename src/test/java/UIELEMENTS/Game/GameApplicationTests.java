4714d3e536676f7b5f5a71cde2cf3c63e06d8d5c
ali <70173146+ari3ky@users.noreply.github.com>
2022-12-22 18:21:45 +0000

    wrote actor class test

--- src/test/java/UIELEMENTS/Game/GameApplicationTests.java
+++ src/test/java/UIELEMENTS/Game/GameApplicationTests.java
@@ -1,13 +1,22 @@
 package UIELEMENTS.Game;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+
 class GameApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void testGettersAndSetters(){
+		Actor actor1 = new Actor();
+		actor1.setActorid(5);
+		Assertions.assertEquals(5,actor1.getActorid(), "Actor id error");
+		actor1.setFirstname("Firstnametest");
+		Assertions.assertEquals("Firstnametest",actor1.getFirstname(), "First name error");
+		actor1.setLastname("Lastnametest");
+		Assertions.assertEquals("Lastnametest",actor1.getLastname(), "Last name error");
 	}
+
 
 }
